fb46caa948ce3f206255a1ae1fcd2c425ce37f76
Ravi Kumar Bharti <43597318+rkbharti@users.noreply.github.com>
2025-06-10 07:45:40 +0000

    update changes in TextForm

--- textutils/src/customComponents/Textform.js
+++ textutils/src/customComponents/Textform.js
@@ -31,19 +31,26 @@ export default function Textform(props) {
                         rows="8"
                     ></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpclick}>
+                <button className="btn btn-primary my-3" onClick={handleUpclick}>
                     Convert to UpperCase
                 </button>
-                <button className="btn btn-primary mx-2" onClick={handleDownclick}>
+                <button className="btn btn-primary my-3" onClick={handleDownclick}>
                     Convert to LowerCase
                 </button>
             </div>
-            <div className='container my-5'>
+            <div className='container my-3'>
                 <h1>Text Summary</h1>
-                <p> {text.split(" ").length} and {text.length}</p>
-                <p> {0.008 * text.split(" ").length }Minutes</p>
-                <h2>Preview</h2>
-                <p>{text}</p>
+               <p> {text.split(" ").length} words and {text.length} chracters</p>
+            </div>
+
+            <div className='container my-3'>
+                <h2>Minutes taken to Read this paragraph</h2>
+               <p> {0.008 * text.split(" ").length }minutes taken </p>
+            </div>
+
+            <div className='container my-3'>
+                <h3>PREVIEW of Text</h3>
+               <p> {text} </p>
             </div>
         </>
     )

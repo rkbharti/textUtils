f5d824ec00cf8f216375a2ce2cf680d6ec2bde7f
Ravi Kumar Bharti <43597318+rkbharti@users.noreply.github.com>
2025-06-11 17:27:25 +0000

    ADD REPLACE TEXT AND CLEAR TEXT BUTTON

--- textutils/src/customComponents/Textform.js
+++ textutils/src/customComponents/Textform.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 export default function Textform(props) {
     const [text, setText] = useState('enter custom input');
+    const [findText , setFindText] = useState('');
+    const [replaceText , setReplaceText] = useState('');
 
     const handleUpclick = () => {
         let convertedUpperCase = text.toUpperCase();
@@ -14,6 +16,21 @@ export default function Textform(props) {
         setText(convertedLowerCase);
     }
 
+    const handleSplitclick = () => {
+        let convertedSplitText = text.split(" ");
+        setText(convertedSplitText.join("\n"));
+    }
+
+     const handleReplaceclick = () => {
+        const updatedText = text.replaceAll(findText ,replaceText);
+        setText(updatedText);
+    }
+
+    const handleClearclick = () => {
+        const clearText = ("");
+        setText(clearText);
+    }
+
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
@@ -23,6 +40,20 @@ export default function Textform(props) {
             <div className='container my-3'>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
+                <input
+                    type='text'
+                    className="form-control my-2"
+                    placeholder="Text To Find"
+                    value={findText}
+                    onChange={(e)=> setFindText(e.target.value)} 
+                    />
+                <input
+                    type='text'
+                    className= "form-control my-2"
+                    placeholder= "Text to replace with"
+                    value={replaceText}
+                    onChange={(e)=> setReplaceText(e.target.value)}
+                    />
                     <textarea 
                         className="form-control" 
                         value={text} 
@@ -31,12 +62,23 @@ export default function Textform(props) {
                         rows="8"
                     ></textarea>
                 </div>
-                <button className="btn btn-primary my-3" onClick={handleUpclick}>
+                <button className="btn btn-primary mx-3" onClick={handleUpclick}>
                     Convert to UpperCase
                 </button>
-                <button className="btn btn-primary my-3" onClick={handleDownclick}>
+                <button className="btn btn-primary mx-3" onClick={handleDownclick}>
                     Convert to LowerCase
                 </button>
+                <button className="btn btn-primary mx-3" onClick={handleSplitclick}>
+                    Split Text
+                </button>
+                <button className="btn btn-primary mx-3" onClick={handleReplaceclick}>
+                    Replace Text
+                </button>
+                 <button className="btn btn-primary mx-3" onClick={handleClearclick}>
+                    Clear Text
+                </button>
+                 
+               
             </div>
             <div className='container my-3'>
                 <h1>Text Summary</h1>
